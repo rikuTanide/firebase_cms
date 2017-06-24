b91e6aca7395843706ebdee1ebfa8c59520a4c9d
rikuTanide <riku@tanide.net>
2017-06-24 23:19:19 +0900

    HTMLをPrint

--- functions/index.ts
+++ functions/index.ts
@@ -1,5 +1,24 @@
 import functions = require( "firebase-functions");
+import fs = require("fs");
 
-exports.index = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
+exports.index = functions.https.onRequest(async (request, response) => {
+    let index = await getFile("index.html");
+    response.send(index);
 });
+
+// async function main(): Promise<any> {
+//     let index = await getFile("index.html");
+//     console.log(index);
+// }
+
+function getFile(path: string): Promise<string> {
+    return new Promise((resolve, error) => {
+        fs.readFile(path, "utf8", (err, data) => {
+            resolve(data);
+        });
+    });
+}
+
+// main().then(() => {
+//     process.exit();
+// });
